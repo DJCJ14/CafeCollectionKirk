c36c493e9fd26e4a95d688e371b6fa9355224134
DJCJ14 <dekfrek@gmail.com>
2022-09-14 08:06:48 -0500

    initial commit

--- CafeCollectionKirk/ViewController.swift
+++ CafeCollectionKirk/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cartOutlet: UILabel!
+    
+    @IBOutlet weak var menuOutlet: UILabel!
+    
+    var item = ["Iced Coffee", "Pastry", "Muffin", "Hot Coffee", "Scone"]
+    var value = [5.25, 2.00, 4.25, 3.00, 5.50]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        menuOutlet.text = "              Menu \n\n\(item[0]),  $\(value[0])\n\(item[1]),  $\(value[1])\n\(item[2]),  $\(value[2])\n\(item[3]),   $\(value[3])\n\(item[4]),  $\(value[4])"
+        
+        
         // Do any additional setup after loading the view.
     }
 
+    
+    
+    
 
 }
 
