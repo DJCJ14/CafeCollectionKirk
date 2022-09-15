6e56103c6a30ba4b12dada0ea2ee71f94d5b2b00
DJCJ14 <dekfrek@gmail.com>
2022-09-15 08:07:59 -0500

    items addable to cart

--- CafeCollectionKirk/ViewController.swift
+++ CafeCollectionKirk/ViewController.swift
@@ -11,21 +11,30 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var cartOutlet: UILabel!
     
-    @IBOutlet weak var menuOutlet: UILabel!
+    @IBOutlet weak var menuOutlet: UITextView!
     
-    var item = ["Iced Coffee", "Pastry", "Muffin", "Hot Coffee", "Scone"]
-    var value = [5.25, 2.00, 4.25, 3.00, 5.50]
+    @IBOutlet weak var cartTextFieldOutlet: UITextField!
+    
+    var menu = ["Bagel":4.99, "Cheese":2.99, "Coffee":3.99, "Scone":4.99, "Apple":1.99]
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        menuOutlet.text = "              Menu \n\n\(item[0]),  $\(value[0])\n\(item[1]),  $\(value[1])\n\(item[2]),  $\(value[2])\n\(item[3]),   $\(value[3])\n\(item[4]),  $\(value[4])"
+        self.menuOutlet.text = "\(menu)"
+        
         
         
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func addAction(_ sender: Any) {
+        let cartItem = String (cartTextFieldOutlet.text!)
+        if(menu.keys.contains(cartItem)){
+            cartOutlet.text = "\(cartItem): $\(menu[cartItem]!)"
+        }
+    }
+    
     
     
     
