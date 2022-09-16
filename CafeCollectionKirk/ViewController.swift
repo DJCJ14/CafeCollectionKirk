cbd589816e82be6905121a882d628fcbf98e1692
DJCJ14 <dekfrek@gmail.com>
2022-09-16 08:08:50 -0500

    nearly finished

--- CafeCollectionKirk/ViewController.swift
+++ CafeCollectionKirk/ViewController.swift
@@ -9,17 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var cartOutlet: UILabel!
+    @IBOutlet weak var cartOutlet: UITextView!
     
     @IBOutlet weak var menuOutlet: UITextView!
     
     @IBOutlet weak var cartTextFieldOutlet: UITextField!
+    
+    @IBOutlet weak var addMenuItemOutlet: UITextField!
+    
+    @IBOutlet weak var adminPasswordOutlet: UITextField!
+    
+    @IBOutlet weak var addRemoveButtonOutlet: UIButton!
+    
+    @IBOutlet weak var totalOutlet: UILabel!
+    
     
     var menu = ["Bagel":4.99, "Cheese":2.99, "Coffee":3.99, "Scone":4.99, "Apple":1.99]
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        addMenuItemOutlet.isHidden = true
+        addRemoveButtonOutlet.isHidden = true
         
         self.menuOutlet.text = "\(menu)"
         
@@ -29,11 +41,31 @@ class ViewController: UIViewController {
     }
 
     @IBAction func addAction(_ sender: Any) {
+        var total = Double (0)
         let cartItem = String (cartTextFieldOutlet.text!)
         if(menu.keys.contains(cartItem)){
-            cartOutlet.text = "\(cartItem): $\(menu[cartItem]!)"
+            cartOutlet.text =  "\(cartOutlet.text!)\n \(cartItem): $\(menu[cartItem]!)"
+            total = total + (menu[cartItem]!)
+            totalOutlet.text = "\(total)"
+            
         }
     }
+    
+    
+    
+    @IBAction func enterPasswordAction(_ sender: UIButton) {
+        let password = String (adminPasswordOutlet.text!)
+        if(password == "12345"){
+        addMenuItemOutlet.isHidden = false
+            addRemoveButtonOutlet.isHidden = false
+    }
+    }
+    
+    @IBAction func addRemoveButton(_ sender: Any) {
+        
+    }
+    
+        
     
     
     
